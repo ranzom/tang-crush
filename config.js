2309390c74560cf7191ea5391c5e5cb64021d392
ranzom <66833368+ranzom@users.noreply.github.com>
2021-06-03 15:32:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Cục nợ đáng iu cute xỉu của anh à',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'hehe',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Làm vợ anh nhé bbi',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/RanZom.13014' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
